aa82e2e741efcb627cde7cb1fbaac7b3c46b1d88
Richard Hoffman <coverslide@gmail.com>
2025-10-20 07:15:17 -0500

    feat: Reorder main menu alphabetically

--- web/src/elements/MainMenu.ts
+++ web/src/elements/MainMenu.ts
@@ -15,16 +15,16 @@ CustomElement.register(
   `
 <ul>
   <li><a route href="/">Home</a></li>
-  <li><a route href="/json">JSON Tool</a></li>
   <li><a route href="/base64">Base64 Tool</a></li>
+  <li><a route href="/json">JSON Tool</a></li>
+  <li><a route href="/markdown">Markdown Tool</a></li>
+  <li><a route href="/portscan">Port Scan Tool</a></li>
+  <li><a route href="/qrcode">QR Code Tool</a></li>
+  <li><a route href="/request">Request Tool</a></li>
+  <li><a route href="/stopwatch">Stopwatch Tool</a></li>
   <li><a route href="/timestamp">Timestamp Tool</a></li>
   <li><a route href="/timer">Timer Tool</a></li>
-  <li><a route href="/stopwatch">Stopwatch Tool</a></li>
-  <li><a route href="/portscan">Port Scan Tool</a></li>
-  <li><a route href="/request">Request Tool</a></li>
-  <li><a route href="/qrcode">QR Code Tool</a></li>
   <li><a route href="/uuid">UUID Generator</a></li>
-  <li><a route href="/markdown">Markdown Tool</a></li>
 </ul>
 `,
 );
